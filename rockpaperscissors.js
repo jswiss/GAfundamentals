ffd5c57c65eb8a9266ecebff6cfec606e733af53
Joshua <swissjoshua@gmail.com>
2015-07-14 23:02:10 +0300

    Unit 4 homework

---
+++ rockpaperscissors.js
@@ -0,0 +1,73 @@
+////////////////////////////////////////////////
+/*   Provided Code - Please Don't Edit   */
+////////////////////////////////////////////////
+'use strict';
+
+function getInput() {
+    console.log("Please choose either 'rock', 'paper', or 'scissors'.")
+    return prompt();
+}
+function randomPlay() {
+    var randomNumber = Math.random();
+    if (randomNumber < 0.33) {
+        return "rock";
+    } else if (randomNumber < 0.66) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+////////////////////////////////////////////////
+/*           Write Your Code Below            */
+////////////////////////////////////////////////
+
+function getPlayerMove(move) {
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `getInput()`.
+    return move || getInput();
+}
+
+function getComputerMove(move) {
+    // Write an expression that operates on a variable called `move`
+    // If a `move` has a value, your expression should evaluate to that value.
+    // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
+    return move || randomPlay();
+}
+
+function getWinner(playerMove,computerMove) {
+    var winner;
+   		if (playerMove === computerMove) {
+   			return('tie');
+		}; 
+		else if (playerMove = 'rock'){
+   			if (computerMove = 'scissors') {
+   				return('player');
+   			} else {
+   				return('computer');
+   			}
+   		if (playerMove = 'paper'){
+   			if (computerMove = 'rock') {
+   				return('player');
+   			} else {
+   				return('computer');
+   			}
+		if (playerMove = 'scissors'){
+   			if (computerMove = 'paper') {
+   				return('player');
+   			} else {
+   				return('computer');
+   			}
+
+    return winner;
+}
+
+function playToFive() {
+    console.log("Let's play Rock, Paper, Scissors");
+    var playerWins = 0;
+    var computerWins = 0;
+    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
+    /* YOUR CODE HERE */
+    return [playerWins, computerWins];
+}
+
