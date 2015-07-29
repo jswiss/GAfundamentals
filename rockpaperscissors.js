4a4b51b9d84f0dcc7a26a088ad1fffc9cf85c399
Joshua <swissjoshua@gmail.com>
2015-07-29 21:53:58 +0300

    likely bad code with while loop in playToFive()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -38,36 +38,45 @@ function getComputerMove(move) {
 function getWinner(playerMove,computerMove) {
     var winner;
    		if (playerMove === computerMove) {
-   			console.log('tie');
+   			winner = 'tie';
 		}; 
 		else if (playerMove = 'rock'){
    			if (computerMove = 'scissors') {
-   				console.log('player');
+   				winner = 'player';
    			} else {
-   				console.log('computer');
+   				winner = 'computer';
    			}
    		if (playerMove = 'paper'){
    			if (computerMove = 'rock') {
-   				console.log('player');
+   				winner = 'player';
    			} else {
-   				console.log('computer');
+   				winner = 'computer';
    			}
 		if (playerMove = 'scissors'){
    			if (computerMove = 'paper') {
-   				console.log('player');
+   				winner = 'player';
    			} else {
-   				console.log('computer');
+   				winner = 'computer';
    			}
 
     return winner;
 }
 
 function playToFive() {
-    console.log("Let's play Rock, Paper, Scissors");
-    var playerWins = 0;
-    var computerWins = 0;
-    // Write code that plays 'Rock, Paper, Scissors' until either the player or the computer has won five times.
-    /* YOUR CODE HERE */
-    return [playerWins, computerWins];
+  console.log('Let\'s play Rock Paper Scissors');
+  var playerWins = 0;
+  var computerWins = 0;
+  console.log('Player chose ' + playerMove + ' while Computer chose ' + computerMove);
+  console.log('The score is currently ' + playerWins + ' to ' + computerWins + '\n');
+  	while (playerWins < 5 && computerWins < 5) {
+  		if (winner = 'player') {
+  			playerWins += 1;
+  		}
+  		if (winner = 'computer') {
+  			computerWins += 1;
+  		};
+  	}
+
+  return [playerWins, computerWins];
 }
 
